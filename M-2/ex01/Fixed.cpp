5ac5e3c80254dcaa76f07220d8265230a557fc70
Hudson512 <hmateque@gmail.com>
2025-03-22 09:34:36 +0100

    ex01 terminado

--- M-2/ex01/Fixed.cpp
+++ M-2/ex01/Fixed.cpp
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
@@ -6,14 +6,23 @@
 /*   By: hmateque <hmateque@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 08:35:25 by hmateque          #+#    #+#             */
-/*   Updated: 2025/03/12 12:28:52 by hmateque         ###   ########.fr       */
+/*   Updated: 2025/03/22 09:19:40 by hmateque         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "Fixed.hpp"
 
+// Constructores
 Fixed::Fixed() : _value(0){
     std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) : _value(value << _fractionalBits){
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : _value(roundf(value * (1 << _fractionalBits))){
+    std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& fixed)
@@ -21,6 +30,8 @@ Fixed::Fixed(const Fixed& fixed)
     std::cout << "Copy constructor called" << std::endl;
     *this = fixed;
 }
+
+// Operadores
 
 Fixed& Fixed::operator=(const Fixed& fixed)
 {
@@ -30,13 +41,30 @@ Fixed& Fixed::operator=(const Fixed& fixed)
     return *this;
 }
 
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
+{
+    os << fixed.toFloat();
+    return os;
+}
+
+// Destructor
 Fixed::~Fixed(){
     std::cout << "Destructor called" << std::endl;
 }
 
+// Metodos
+float Fixed::toFloat(void) const
+{
+    return (float)this->_value / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return this->_value >> _fractionalBits;
+}
+
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
     return this->_value;
 }
 
--- M-2/ex01/Fixed.hpp
+++ M-2/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hmateque <hmateque@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 08:35:43 by hmateque          #+#    #+#             */
-/*   Updated: 2025/03/22 07:58:42 by hmateque         ###   ########.fr       */
+/*   Updated: 2025/03/22 09:17:13 by hmateque         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,6 +14,7 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
@@ -21,14 +22,20 @@ private:
     int _value;
     static const int _fractionalBits = 8;
 public:
-    Fixed();
+    Fixed(const int value);
+    Fixed(const float value);
     Fixed(const Fixed& fixed);
+    Fixed();
+    float toFloat(void) const;
+    int toInt(void) const;
+    
     Fixed& operator=(const Fixed& fixed);
+    
     ~Fixed();
     int getRawBits(void) const;
     void setRawBits(int const raw);
 };
 
-
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
 
 #endif
